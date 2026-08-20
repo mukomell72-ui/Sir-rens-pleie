d7e720ed1510a34dc73939e6218406c66ed442e6
mukomell72-ui <mukomell72@gmail.com>
2026-08-20 22:28:27 +0200

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='sir-reference-v1';
+const ASSETS=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
